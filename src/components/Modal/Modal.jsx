a5986a4eff7b995c77d0fe3d48faed50075b34cb
rafaelrvs <rafaelvieira1333@gmail.com>
2024-09-17 13:31:08 -0300

    Adicionando descrição Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -14,61 +14,88 @@ export const Modal = () => {
             setProdutoModal(pedidoSelecionado.produtos);
         }
     }, [pedidoSelecionado]); // Atualiza quando o pedidoSelecionado mudar
-console.log(produtoModal);
-console.log(produtoModal);
+    console.log(produtoModal);
+    console.log(produtoModal);
 
 
     return (
         <div className={styles.containerModal}>
             <p><span onClick={() => setModal(false)}>X</span></p>
             <div className={styles.containerPedidoHeader}>
-                <p>Pedido ID:<br /> {pedidoSelecionado.id}</p>
-                <p>Status:<br /> {pedidoSelecionado.status}</p>
-                <p>Quantidade Total:<br />{pedidoSelecionado.qtdeTotal}</p>
-                <p>Valor Total:<br /> {pedidoSelecionado.valorTotal}</p>
+                <p>  <strong> Pedido ID:   </strong> {pedidoSelecionado.id}</p>
+                <p>  <strong> Status:   </strong> {pedidoSelecionado.status}</p>
+                <p>  <strong> Quantidade Total:  </strong> {pedidoSelecionado.qtdeTotal}</p>
+                <p>  <strong> Valor Total:   </strong> {pedidoSelecionado.valorTotal}</p>
 
             </div>
+
+
             <hr className={styles.hr} />
+
+
             <div className={styles.containerContent}>
                 <div>
-
-                <h3>Item</h3>
-                {produtoModal.map((item, index) => (
-                    <div key={index} className={styles.produtoListado}>
-                       <div>
-                        Codigo:
-                             <p>{item.cod}</p>
-                        </div> 
-                       <div>
-                        Tamanho:
-                             <p>{item.tamanho}</p>
-                        </div> 
-                       <div>
-                        Cor:
-                             <p>{item.cor}</p>
-                        </div> 
-                       <div>
-                        Quantidade:
-                             <p>{item.quantidade}</p>
-                        </div> 
-                       <div>
-                        Preço R$
-                             <p>{item.preco}</p>
-                        </div> 
-                        <img 
-                            src={`/src/images/produtos/${item.img}`}
-                            alt="aaaa" 
-                            className={styles.image}
-                        />
-                    </div>
-                ))}
+                        <br />
+                    <h2>Item</h2>
+                    {produtoModal.map((item, index) => (
+                        <div key={index} className={styles.produtoListado}>
+                            <div>
+                                Codigo:
+                                <p>{item.cod}</p>
+                            </div>
+                            <div>
+                                Tamanho:
+                                <p>{item.tamanho}</p>
+                            </div>
+                            <div>
+                                Cor:
+                                <p>{item.cor}</p>
+                            </div>
+                            <div>
+                                Quantidade:
+                                <p>{item.quantidade}</p>
+                            </div>
+                            <div>
+                                Preço R$
+                                <p>{item.preco}</p>
+                            </div>
+                            <img
+                                src={`/src/images/produtos/${item.img}`}
+                                alt="aaaa"
+                                className={styles.image}
+                            />
+                        </div>
+                    ))}
 
                 </div>
 
 
                 <div>
-                    <h1>Detalhe</h1>
+                    <br />
+                    <h2>Detalhe</h2>
                     <div className={styles.containerDetalhe} >
+                        <h3>Acompanhamento</h3>
+                        <hr className={styles.hrDetalhes} />
+
+                        <div className={styles.statusPedido}>
+                            <p className={styles.status2}>
+                            
+                            </p>
+                            <p className={styles.status3}>
+
+                            </p>
+                            <p className={styles.status4}>
+
+                            </p>
+                            <p className={styles.status5}>
+
+                            </p>
+
+                            <hr className={styles.LinhaTempo} />
+
+
+                        </div>
+                       <p> Aprovação Produção Envio Entrega</p>
 
                     </div>
 
--- src/components/Pages/AcompanhaPedido/AcompanhaPedido.jsx
+++ src/components/Pages/AcompanhaPedido/AcompanhaPedido.jsx
@@ -6,65 +6,72 @@ import { Modal } from '../../Modal/Modal';
 
 export const AcompanhaPedido = () => {
 
-    const {pedidos, setPedidos} = useContext(GlobalContext);
-    const {setModal} = useContext(GlobalContext)
-    const {setValueModal} = useContext(GlobalContext)
+    const { pedidos, setPedidos } = useContext(GlobalContext);
+    const { setModal } = useContext(GlobalContext)
+    const { setValueModal } = useContext(GlobalContext)
 
 
 
-function handleActiveModal(e) {
-  
+    function handleActiveModal(e) {
+
         setValueModal(Number(e.target.value));
-        
-
-   setModal(true);   
-}
 
 
-  return (
-    <div className={styles.ContainrePedidos}>
-   
-          
-        {pedidos.length > 0 && pedidos.map((pedido)=>(
-            <div key={pedido.id} className={styles.containerPedido}>
-                <div className={styles.itensPedidoHeader} >
+        setModal(true);
+    }
 
-                    <div>
-                       <span>
+
+    return (
+        <div className={styles.ContainrePedidos}>
+
+
+            {pedidos.length > 0 && pedidos.map((pedido) => (
+                <div key={pedido.id} className={styles.containerItensDoPedido}>
+
+
+                    <span>
+                        <strong>
+
                         Nº Pedido
-                        </span> 
-                    {pedido.id}    
-                    </div>
-                    <div>
-                       <span>
+                        </strong>
+                        {pedido.id}
+                    </span>
+                    <span>
+                        <strong>
+
                         Data
-                       </span>
-                        {pedido.data}
-                    </div>
-                    <div>
-                        <span>
+                        </strong>
+                      {pedido.data}
+                
+                    </span>
+                    <span>
+                        <strong>
 
                         Status
-                        </span>
-                        {pedido.status}
-                    </div>
-                        <div>
+                        </strong>
+                  
+                    {pedido.status}
+                
+                    </span>
+
                     <span>
+                        <strong>
 
                         Valor
+                        </strong>
+                    R${" " + parseFloat(pedido.valorTotal).toFixed(2)}
                     </span>
-                    R${ " " + parseFloat(pedido.valorTotal).toFixed(2)}
-                        </div>
+
 
                     <button className={styles.btnVerItens} value={pedido.id} onClick={handleActiveModal} >Ver itens</button>
-                    </div>
-                 
+                </div>
 
-                
-       
-            </div>
-        ))}
-    
-    </div>
-  )
+
+
+
+
+            ))}
+
+        </div>
+    )
 }
