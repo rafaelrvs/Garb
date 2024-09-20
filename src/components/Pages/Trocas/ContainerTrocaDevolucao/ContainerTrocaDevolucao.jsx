a0cfd2b2f70cd49d8d0aa4124aa25f8c63c4c348
Felipe Johnny <felipijohnny@outlook.com>
2024-09-20 16:15:46 -0300

    iniciando estilização em trocas e devolucao

---
+++ src/components/Pages/Trocas/ContainerTrocaDevolucao/ContainerTrocaDevolucao.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './ContainerTrocaDevolucao.module.css'
+const ContainerTrocaDevolucao = () => {
+  return (
+    <div className={styles.containerLista}>
+      a
+    </div>
+  )
+}
+
+export default ContainerTrocaDevolucao
--- src/components/Pages/Trocas/Trocas.jsx
+++ src/components/Pages/Trocas/Trocas.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Trocas.module.css'
+import {NavLink} from 'react-router-dom'
+import ContainerTrocaDevolucao from './ContainerTrocaDevolucao/ContainerTrocaDevolucao'
 const Trocas = () => {
+
+  const [active, setActive]=useState(false)
+  function handleSelect(){
+    setActive(!active)
+  }
+
   return (
-    <div className={styles.container}>
+    <section className={styles.container}>
+      <h1 className={styles.tituloPage}>Trocas e Devoluções</h1>
+      <div className={styles.LinhaStyle}></div>
+      <h3 className={styles.subTitle}>Selecione a ação desejada para prosseguir</h3>
+      <div className={styles.wrapperButtons}>
+        <NavLink  className={styles.navButton} onClick={handleSelect}>Troca</NavLink>
+        <NavLink  className={styles.navButton}onClick={handleSelect}>Devolução</NavLink>
+      </div>
+      {active&& <ContainerTrocaDevolucao/>}
       
-    </div>
+    </section>
   )
 }
 
