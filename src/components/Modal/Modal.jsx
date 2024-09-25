d67630d5c6382b5de4c5f71c881becc985a3ca32
rafaelrvs <rafaelvieira1333@gmail.com>
2024-09-25 12:14:12 -0300

    Adicionando acompanhamento de produto

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,14 +1,13 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import styles from './Modal.module.css';
 import { GlobalContext } from '../../Context/GlobalContext';
-
-
+import CryptoJS from 'crypto-js';
 export const Modal = () => {
-    const { setModal, pedidos, valueModal} = useContext(GlobalContext);
+    const { setModal, pedidos, valueModal } = useContext(GlobalContext);
     const [produtoModal, setProdutoModal] = useState([]);
     // Filtrando o pedido com o ID correspondente a valueModal
     const pedidoSelecionado = pedidos.find(e => e.id === valueModal);
-
+    
     const voltar = useRef()
     function closeModal(event) {
         if (event.currentTarget === voltar.current) {
@@ -16,10 +15,17 @@ export const Modal = () => {
         }
     }
 
+
+    
+    const mensagem = `seu id ${pedidoSelecionado.id} cor ${pedidoSelecionado.cor}`;
+    const chave = CryptoJS.enc.Hex.parse('0123456789abcdef0123456789abcdef');
+    const vi = CryptoJS.enc.Hex.parse('abcdef9876543210abcdef9876543210');
+    const mensagemCifrada = CryptoJS.AES.encrypt(mensagem, chave, { iv: vi }).toString();
     useEffect(() => {
         if (pedidoSelecionado) {
             setProdutoModal(pedidoSelecionado.produtos);
         }
+        console.log(pedidoSelecionado);
     }, [pedidoSelecionado]); // Atualiza quando o pedidoSelecionado mudar
 
 
@@ -31,6 +37,7 @@ export const Modal = () => {
         <div className={styles.containerModal}>
             <div className={styles.modal}>
                 <div className={styles.contentInModal}>
+
                     <div className={styles.headerModalPedidos}>
 
                         <div className={styles.voltarContainer} onClick={closeModal} ref={voltar}>
@@ -45,28 +52,66 @@ export const Modal = () => {
 
 
                     </div>
+               
 
 
+                    <div className={styles.containerListaProduto} > 
 
-                    <div className={styles.contentMainModal} >
 
-                        {produtoModal.map((item, index) => (
+                        <div className={styles.contentMainModal} >
+                        <div className={styles.titleListaProduct}>
+                        <strong className={styles.titlimg}>Imagem</strong>
+                        <strong className={styles.title}>Codigo:</strong>
+                        <strong className={styles.title}> Tamanho:</strong>
+                        <strong className={styles.title}>Cor:</strong>
+                        <strong className={styles.title}>Quantidade:</strong>
+                        <strong className={styles.title}>Preço:</strong>
 
-                            <div key={index} className={styles.produltoDaLista}>
-                                <div className={styles.containerImgModal}>
-                                    <img className={styles.image} src={`/images/produtos/${item.img}`} alt="aaa" />
+                    </div>
+
+                            {produtoModal.map((item, index) => (
+
+                                <div key={index} className={styles.produltoDaLista}>
+                                    <div className={styles.containerImgModal}>
+                                        <img className={styles.image} src={`/images/produtos/${item.img}`} alt="aaa" />
+                                    </div>
+
+                                    <p className={styles.itemListaProd}>{item.cod} </p>
+                                    <p className={styles.itemListaProd}>{item.tamanho} </p>
+                                    <p className={styles.itemListaProd}>{item.cor}</p>
+                                    <p className={styles.itemListaProd}>{item.quantidade} </p>
+                                    <p className={styles.itemListaProd}>{item.preco} </p>
+
+                                 
                                 </div>
 
-                                <p className={styles.itemListaProd}><strong  > Codigo:</strong>{item.cod} </p>
-                                <p className={styles.itemListaProd}><strong  > Tamanho:</strong>{item.tamanho} </p>
-                                <p className={styles.itemListaProd}><strong  > Cor:</strong>{item.cor} </p>
-                                <p className={styles.itemListaProd}><strong  >Quantidade:</strong>{item.quantidade} </p>
-                                <p className={styles.itemListaProd}><strong  > R$</strong>{item.preco} </p>
-
+                            ))}
+                            
+                        </div>
+                        <div className={styles.containerAcompanhaProduto}>
+                            <h1 className={styles.titleAcompanhamento}> Acompanhamento</h1>
+                            <div className={styles.campoEstadoDetalhado}>
+                                <span className={styles.itemEstado}></span>
+                                <span className={styles.itemEstado}></span>
+                                <span className={styles.itemEstado}></span>
+                                <span className={styles.itemEstado}></span>
+                                <hr className={styles.barraStatus} />
                             </div>
+                               <div className={styles.statusPed}>
 
-                        ))}
+                                        <p className={styles.legendaStatus} >Aprovado</p>
+                                        <p className={styles.legendaStatus} >Produção</p>
+                                        <p className={styles.legendaStatus} >Em transito</p>
+                                        <p className={styles.legendaStatus} >Entregue</p>
+                                    </div>
+
+                            <div className={styles.footerAcompanhamento} >
+                                <p>Frete:{" " + pedidoSelecionado.frete.preco}</p>
+                                <p className={styles.containerRastreio} >Codigo de rastreio: <p className={styles.codigoRastreio}>{mensagemCifrada}</p></p>
+                            </div>
+                        </div>
                     </div>
+           
                 </div>
 
             </div>
