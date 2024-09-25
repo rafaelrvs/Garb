a2db6964868c03c827c4c62a88acf967cba8395b
Felipe Johnny <felipijohnny@outlook.com>
2024-09-25 08:34:34 -0300

    ajustando renderização dos botoes da sizeBay

--- src/components/Pages/Pedidos/Produto/Produto.jsx
+++ src/components/Pages/Pedidos/Produto/Produto.jsx
@@ -1,40 +1,43 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import styles from './Produto.module.css'
-import { empresas } from '../../../../DB/empresas'
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import styles from './Produto.module.css';
+import { empresas } from '../../../../DB/empresas';
 import Quantidade from '../../../Quantidade/Quantidade';
 import Grade from '../../../Grade/Grade';
 import { GlobalContext } from '../../../../Context/GlobalContext';
+
 const Produto = ({ cargoID }) => {
   const [produtos, setProdutos] = useState([]);
   const [currtenProduto, setCurrentProduto] = useState(0);
-  const [direction, setDirection] = useState(''); // Estado para controlar a direção da animação
-  const {carrinho,setCarrinho,quantidade, setPopUp,popupTimeoutRef,setQuantidade, tamanhoSelecionado} = useContext(GlobalContext);
-  const[gradeAnimacao,setGradeAnimacao] = useState(false)
-  
-  const sizeBay=useRef();
-  const currentProdForSizebay=useRef();
+  const [direction, setDirection] = useState(''); 
+  const { carrinho, setCarrinho, quantidade, setPopUp, popupTimeoutRef, setQuantidade, tamanhoSelecionado } = useContext(GlobalContext);
+  const [gradeAnimacao, setGradeAnimacao] = useState(false);
+
+  const sizeBay = useRef();
+  const sizeBayInitialized = useRef(false);  // Novo ref para controlar a inicialização
+  const currentProdForSizebay = useRef();
 
   useEffect(() => {
     if (sizeBay.current && produtos.length > 0) {
-      // Certificando de que a função SizebayInitLookbook está disponível
-      setTimeout(()=>{
-        if(direction === 'right' || 'left'){
-          setDirection('')          
+      setTimeout(() => {
+        if (direction === 'right' || 'left') {
+          setDirection('');          
         }
-      },350)
+      }, 350);
 
-      async function initSizebay(){
-        if (typeof window.SizebayInitLookbook === 'function') {
-          SizebayInitLookbook();
-        } else {
-          console.error('Função SizebayInitLookbook não encontrada');
+      async function initSizebay() {
+        if (!sizeBayInitialized.current) { // Executa apenas se não foi inicializado
+          if (typeof window.SizebayInitLookbook === 'function') {
+            SizebayInitLookbook();
+            sizeBayInitialized.current = true; // Marca como inicializado
+          } else {
+            console.error('Função SizebayInitLookbook não encontrada');
+          }
         }
-      };
+      }
       initSizebay();
     }
-  }, [produtos,direction]);
-  
-  
+  }, [produtos, direction]);
+
   useEffect(() => {
     const empId = window.localStorage.getItem('currentEmpresa');
     const empresa = empresas.filter((empresa) => empresa.id == empId);
@@ -49,6 +52,7 @@ const Produto = ({ cargoID }) => {
       return;
     }
     setProdutos(prod[0].produtos);
+    sizeBayInitialized.current = false;  // Reseta a inicialização ao trocar de cargo
   }, [cargoID]);
 
   if (!produtos.length) {
@@ -56,99 +60,25 @@ const Produto = ({ cargoID }) => {
   }
 
   const handlePrevious = () => {
-    if (currtenProduto > 0) { // Verifica se há um produto anterior
-      setDirection('left'); // Define a direção da animação como da esquerda
+    if (currtenProduto > 0) {
+      setDirection('left');
       setCurrentProduto((current) => current - 1);
+      sizeBayInitialized.current = false;  // Marca como não inicializado para o novo produto
     } else {
       console.warn("Não há mais produtos anteriores!");
     }
   };
-  
+
   const handleNext = () => {
-    if (currtenProduto < produtos.length - 1) { // Verifica se há um próximo produto
-      setDirection('right'); // Define a direção da animação como da direita
+    if (currtenProduto < produtos.length - 1) {
+      setDirection('right');
       setCurrentProduto((current) => current + 1);
+      sizeBayInitialized.current = false;  // Marca como não inicializado para o novo produto
     } else {
       console.warn("Não há mais produtos à frente!");
     }
   };
-  
-  const handleADDProduct = (selectedSize) => { 
-    
-    if(tamanhoSelecionado !== ""){
-    setGradeAnimacao(false)
 
-    
-    if (!produtos[currtenProduto]) {
-      console.error("Produto não encontrado!");
-      return;
-    }
-    
-    
-    
-    // Verifica se o carrinho é válido
-    const carrinhoAtual = carrinho || [];
-    
-    // Verifica se o produto com o mesmo código e tamanho já existe no carrinho
-    const produtoExistente = carrinhoAtual.find(
-      (item) =>
-        item.cod === produtos[currtenProduto].codigo &&
-      item.tamanho === selectedSize // Verifica também o tamanho
-      );
-      
-      if (produtoExistente) {
-        // Se o produto já estiver no carrinho com o mesmo tamanho, atualiza a quantidade
-      const novoCarrinho = carrinhoAtual.map((item) =>
-        item.cod === produtos[currtenProduto].codigo && item.tamanho === selectedSize
-          ? { ...item, quantidade: item.quantidade + quantidade } // Atualiza a quantidade
-          : item
-        );
-      setCarrinho(novoCarrinho);
-    } else {
-      // Se for um novo produto ou um tamanho diferente, adiciona ao carrinho
-      const novoCarrinho = [
-        ...carrinhoAtual,
-        {
-          cod: produtos[currtenProduto].codigo,
-          descricao: produtos[currtenProduto].descricao,
-          preco: produtos[currtenProduto].preco,
-          cor: produtos[currtenProduto].cor,
-          img:produtos[currtenProduto].img,
-          tamanho: selectedSize, // Adiciona o tamanho selecionado
-          quantidade: quantidade, // Adiciona a quantidade selecionada
-        },
-      ];
-      setCarrinho(novoCarrinho);
-    }
-    
-    setPopUp({
-      status: true,
-      color: "#46bba2",
-      children: "Produto Adicionado ao Carrinho",
-    });
-    
-    if (popupTimeoutRef.current) {
-      clearTimeout(popupTimeoutRef.current);
-    }
-    
-    popupTimeoutRef.current = setTimeout(() => {
-      setPopUp({
-        status: false,
-        color: "",
-        children: "",
-      });
-      popupTimeoutRef.current = null;
-    }, 3000);
-  
-    setQuantidade(1);
-  }else{
-    setGradeAnimacao(true)
-  }
-
-
-  
-  };
-  
   return (
     <div className={styles.containerProduto}>
       <div 
@@ -170,17 +100,13 @@ const Produto = ({ cargoID }) => {
               <span className={styles.preco}>R$ {produtos[currtenProduto].preco}</span>
             </div>
             <div className={styles.column2}>           
-              {/*-------------------------------SIZEBAY-------------------------------*/}
               <div className={styles.provador} ref={sizeBay} data-produto={produtos[currtenProduto].codigo}>              
-              
               </div>
               <Grade grade={produtos[currtenProduto].grade} gradeAnimacao={gradeAnimacao} />
               {gradeAnimacao && <p className={styles.alertGrade}> Selecione um tamanho !</p>}
             </div>
-
           </div>
-            <Quantidade/>
-              <button className={styles.addcarrinho} onClick={() => handleADDProduct(tamanhoSelecionado)}>Adicionar Carrinho +</button>
+          <Quantidade />
         </div>
       </div>
       <div className={styles.buttonsProduto}>
