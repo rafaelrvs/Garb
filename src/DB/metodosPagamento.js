f1fde6fe68fd1311bf902ff767f07a7c0ccc5d32
Felipe Johnny <felipijohnny@outlook.com>
2024-09-25 13:44:09 -0300

    aplicando metodo de pagamento na finalização da compra

---
+++ src/DB/metodosPagamento.js
@@ -0,0 +1,66 @@
+const metodosPagamento = [
+    {
+        "id": 1,
+        "nome": "1X - 30 DIAS",
+        "acrescimo": 0,
+    },
+    {
+        "id": 2,
+        "nome": "2X - 60 DIAS",
+        "acrescimo": 0.02,
+        
+    },
+    {
+        "id": 3,
+        "nome": "3X - 90 DIAS",
+        "acrescimo": 0.025,
+    },
+    {
+        "id": 4,
+        "nome": "4X - 120 DIAS",
+        "acrescimo": 0.029,
+    },
+    {
+        "id": 5,
+        "nome": "5X - 150 DIAS",
+        "acrescimo": 0.030,
+    },
+    {
+        "id": 6,
+        "nome": "6X - 180 DIAS",
+        "acrescimo": 0.036,
+    },
+    {
+        "id": 7,
+        "nome": "7X - 180 DIAS",
+        "acrescimo": 0.039,
+    },
+    {
+        "id": 8,
+        "nome": "8X - 180 DIAS",
+        "acrescimo": 0.040,
+    },
+    {
+        "id":9,
+        "nome": "9X - 180 DIAS",
+        "acrescimo": 0.045,
+    },
+    {
+        "id": 10,
+        "nome": "10X - 180 DIAS",
+        "acrescimo": 0.050,
+    },
+    {
+        "id": 11,
+        "nome": "11X - 180 DIAS",
+        "acrescimo": 0.055,
+    },
+    {
+        "id": 12,
+        "nome": "12X - 180 DIAS",
+        "acrescimo": 0.060,
+    },
+]
+
+
+export default metodosPagamento
--- src/components/Pages/Pedidos/Carrinho/Carrinho.jsx
+++ src/components/Pages/Pedidos/Carrinho/Carrinho.jsx
@@ -5,12 +5,18 @@ import { Link, useNavigate } from 'react-router-dom';
 import QuantidadeFPedido from '../../../QuantidadeFPedido/QuantidadeFPedido';
 import Voltar from '../../../Voltar/Voltar';
 import fretes from '../../../../DB/frete.js'
-
+import metodosPagamento from '../../../../DB/metodosPagamento.js'
+import checkSVG from '/images/check.svg'
+import crossSVG from '/images/cross.svg'
 
 const Carrinho = () => {
   const { carrinho, quantidades } = useContext(GlobalContext);
   const [qtdeTotal, setQtdeTotal] = useState(0);
   const [valorTotal, setValorTotal] = useState(0);
+  const [formaPagSelecionado, setFormaPagSelecionado] = useState({
+    selecionado: false,
+    data:{}
+  })
   const [campoFrete, setcampoFrete] = useState({
     status: false,
     freteID: "",
@@ -20,11 +26,12 @@ const Carrinho = () => {
   });
   const [metPagamento,setMetPagamento]=useState({
     status:false,
-    data:{}
   })
   const [somaFrete, setSomaFrete] = useState(false)
   const [error, setError] = useState(false);
-  const navigate = useNavigate()
+  const [metodoSelecionado, setMetodoSelecionado] = useState(null);
+
+  const navigate = useNavigate();
 
   // Função para gerar um ID aleatório de 4 dígitos
   const generateOrderId = () => {
@@ -32,7 +39,7 @@ const Carrinho = () => {
   };
 
   function handleSubmit() {
-    if(campoFrete.status){
+    if(campoFrete.status && formaPagSelecionado.selecionado){
       const carrinhoAtualizado = carrinho.map(produto => ({
         ...produto,
         quantidade: quantidades[`${produto.cod}-${produto.tamanho}`] || produto.quantidade,
@@ -48,7 +55,12 @@ const Carrinho = () => {
         frete: {
           nomeFrete: campoFrete.nome,
           preco: campoFrete.preco,
+        },
+        metdPagamento:{
+          nome: formaPagSelecionado.data.nome,
+          acrescimo: formaPagSelecionado.data.acrescimo,
         }
+
         
       };
 
@@ -67,7 +79,6 @@ const Carrinho = () => {
     }
     else{
       setError(true)
-      
       setTimeout(()=>{
         setError(false)
 
@@ -75,12 +86,6 @@ const Carrinho = () => {
     }
   }
 
-function handlePagamento(){
-  setMetPagamento({
-    status: !metPagamento.status,
-    data:{},
-  })
-}
     useEffect(() => {
       const calcularTotais = () => {
         let qtde = 0;
@@ -114,14 +119,32 @@ function handlePagamento(){
     })
   }
 
+  function handlePagamento(){
+    setMetPagamento({status: !metPagamento.status,})
+  }
+
+  const handleMetodoChange = (metodoId,acrescimo,nome) => {
+    setMetodoSelecionado(metodoId);    
+    setMetPagamento({
+      status: !metPagamento.status,
+    })
+    setFormaPagSelecionado({
+      selecionado: true,
+      data:{
+        nome,
+        acrescimo
+      }
+    })
+  };
+
   return (
     <>
       <Voltar />
       <section className={styles.Container}>
         <div className={`${styles.colunaUm} animeLeft`}>
-          {carrinho.length&& !metPagamento.status ? (
-            carrinho.map(produto => (
-              <div key={`${produto.cod}-${produto.tamanho}`} className={`${styles.produto}`}>
+          {carrinho.length&& !metPagamento.status && (
+            carrinho.map((produto,index) => (
+              <div key={`${produto.cod}-${produto.tamanho}` || index} className={`${styles.produto}`}>
                 <img src={`/images/produtos/${produto.img}`} alt="" />
                 <h1 className={styles.nome}>{produto.descricao}</h1>
                 <h1 className={styles.tamanho}>Tamanho: {produto.tamanho}</h1>
@@ -132,22 +155,39 @@ function handlePagamento(){
                 </div>
               </div>
             ))
-          ) : (
-            <p>Sem produtos em seu carrinho</p>
           )}
-          {carrinho.length&& metPagamento.status ?(
+          {metPagamento.status &&(
             <>
-            
-            </>
-          )
-          :<p>Sem produtos em seu carrinho</p>
-          }
+            {metodosPagamento.map((metodo, index) => (
+              <div 
+                className={styles.linhaMetodoPagamento}
+                key={(metodo.id || index)}
+                onClick={() => handleMetodoChange(metodo.id, metodo.acrescimo, metodo.nome)}  
+              >
+                <input
+                  type="radio"
+                  name="metodoPagamento"
+                  value={metodo.id}
+                  checked={metodoSelecionado === metodo.id}
+                  onChange={() => handleMetodoChange(metodo.id, metodo.acrescimo, metodo.nome)}   
+                />
+                <label htmlFor={`metodo-${metodo.id}`} className={styles.textMetodoPagamento}>
+                  {metodo.nome}
+                </label>
+                <span className={styles.textMetodoPagamento}>
+                  R$ {(valorTotal * (1 + metodo.acrescimo)).toFixed(2)}
+                </span>
+              </div>
+            ))}
+          </>
+          )}
         </div>
 
         <div className={styles.colunaDois}>
           <p>Qtde. Total: {qtdeTotal}</p>
           <p>Frete:{campoFrete.preco}</p>
-          <p>Valor Total: R$ {valorTotal.toFixed(2)}</p>
+          <p>forma de pag: {formaPagSelecionado.data.nome || 0}</p>
+          <p>Valor Total: R$ {(valorTotal * (1 + (formaPagSelecionado.data.acrescimo || 0))).toFixed(2)}</p>
 
           <button className={`${styles.btnFinalizar} ${styles.button}`} onClick={handleSubmit} disabled={!carrinho.length}>
             Finalizar Pedido
@@ -158,19 +198,25 @@ function handlePagamento(){
         </div>
 
         {/*metodo de pagamento*/}
-        <div className={styles.containerMetPagamento} onClick={handlePagamento}>
-          <button className={styles.button}>Método de pagamento</button>
-          <img src={metPagamento.status? '':''} alt="status de seleção de pagamento" />
+        <div className={styles.containerMetPagamento} >
+          <button className={styles.button} onClick={handlePagamento}>Método de pagamento</button>
+          <img 
+            src={formaPagSelecionado.selecionado? checkSVG : crossSVG} 
+            alt="status de seleção de pagamento" 
+            className={error&& !formaPagSelecionado.selecionado ? styles.statusFormaPagAnimation : styles.statusFormaPag}          
+          />
+          
         </div>
 
         {/*Frete*/}
-        <div className={error? styles.colunaDoisEmpity:styles.colunaDoisFret}   >
+        <div className={error&& !campoFrete.status ? styles.colunaDoisEmpity:styles.colunaDoisFret}   >
           <h1 className={styles.tituloFrete}>Escolha forma de entrega</h1>
           <div className={styles.containerFretes}>
             {fretes.map((frete) => (
               <div
                 className={campoFrete.status && campoFrete.freteID === frete.id ? styles.contentFreteActive : styles.contentFrete}
                 onClick={() => { activeFrete(frete) }}
+                key={frete.id}
               >
                 <img
                   className={styles.imgTruck}
