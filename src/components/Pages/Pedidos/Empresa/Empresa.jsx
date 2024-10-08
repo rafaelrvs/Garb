c4cfe632e0813cc110a921335a82b0b9d48e3bb9
Felipe Johnny <felipijohnny@outlook.com>
2024-10-08 16:05:36 -0300

    aplicando homepage

--- src/components/Pages/Pedidos/Empresa/Empresa.jsx
+++ src/components/Pages/Pedidos/Empresa/Empresa.jsx
@@ -3,14 +3,18 @@ import styles from './Empresa.module.css'
 import { empresas } from '../../../../DB/empresas'
 import { useNavigate } from 'react-router-dom'
 import Voltar from '../../../Voltar/Voltar'
-
+import CasasBahia from '/images/empresas/CasasBahia.svg'
+import Anac from '/images/empresas/anac.svg'
+import Cbre from '/images/empresas/cbre.svg'
+import Vivo from '/images/empresas/vivo.svg'
+import Claro from '/images/empresas/claro.svg'
 const Empresa = () => {
     const navigate = useNavigate()
-  
     function abrirEmp(empId){
       navigate(`${empId}`)
       window.localStorage.setItem('currentEmpresa',empId)
     }
+
   return (
     <div>
         <Voltar/>
@@ -22,6 +26,15 @@ const Empresa = () => {
                 <div className={`${styles.qtdeCargo}`}><p className={styles.text}>{empresa.cargos.length}</p></div>
                 <span className={`${styles.text}  ${styles.nomeText}`}>{empresa.nome}</span>
                 <span className={styles.text}>CNPJ: {empresa.cnpj}</span>
+                <div className={styles.containerLogo}>
+
+                  {empresa.id ===1&& <img src={CasasBahia} alt="logo" className={styles.logo}/>}
+                  {empresa.id ===2&& <img src={Cbre} alt="logo" className={styles.logo}/>}
+                  {empresa.id ===3&& <img src={Claro} alt="logo" className={styles.logo}/>}
+                  {empresa.id ===4&& <img src={Vivo} alt="logo" className={styles.logo}/>}
+                  {empresa.id ===5&& <img src={Vivo} alt="logo" className={styles.logo}/>}
+                  {empresa.id ===6&& <img src={Anac} alt="logo" className={styles.logo}/>}
+                </div>
               </div>
             </div>
         ))}
